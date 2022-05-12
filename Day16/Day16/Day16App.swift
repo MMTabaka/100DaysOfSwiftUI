b9f254e800dd15e9127c06c61a3e1dbe34b22dbe
MMTabaka <miloszmtabaka@gmail.com>
2022-05-12 22:46:33 +0100

    Day 16 - initial

---
+++ Day16/Day16/Day16App.swift
@@ -0,0 +1,17 @@
+//
+//  Day16App.swift
+//  Day16
+//
+//  Created by Milosz Tabaka on 12/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
